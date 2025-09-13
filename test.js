49c7fdc715ca821d28e651c956b9a0f7ae19a42c
rickey <12867336+HelloRickey@users.noreply.github.com>
2025-09-13 11:24:17 +0800

    fix ts

--- test.js
+++ test.js
@@ -1,21 +1,27 @@
+// Import AmeComponent from local src/index
 import { AmeComponent } from "./src/index";
+// Import function to convert private key to account
 import { privateKeyToAccount } from "viem/accounts";
+// Load environment variables from .env file
 import dotenv from "dotenv/config";
 
+// Define local test network configuration
 const localhost = {
   id: 31337,
   rpcUrls: {
     default: { http: ["http://127.0.0.1:8545"] },
   },
 };
+// Instantiate AmeComponent with network and contract address
 const component = new AmeComponent(
   localhost,
   "0x0E2b5cF475D1BAe57C6C41BbDDD3D99ae6Ea59c7"
 );
 
+// Create account object from private key in environment variables
 var account = privateKeyToAccount(process.env.PRIVATE_KEY);
 
-
+// Query all supported methods and their details from the contract
 async function getComponentMethods() {
   const options = await component.getComponentOptions();
   console.log("support methods:", options);
@@ -37,6 +43,7 @@ async function getComponentMethods() {
   console.log("methods:", methods);
 }
 
+// Send a GET request to fetch user information
 async function sendGetRequest() {
   var encode = component.encodeRequestParams(
     [{ type: "address" }],
@@ -46,6 +53,7 @@ async function sendGetRequest() {
   console.log("getResponse:", getResponse);
 }
 
+// Send a POST request to create a new user
 async function sendPostRequest() {
   var postReqEncode = component.encodeRequestParams(
     [{ type: "string" }, { type: "uint256" }],
@@ -61,6 +69,7 @@ async function sendPostRequest() {
   console.log("postResponse:", postResponse);
 }
 
+// Send a PUT request to update user's name
 async function sendPutRequest() {
   var putReqEncode = component.encodeRequestParams(
     [
@@ -79,6 +88,7 @@ async function sendPutRequest() {
   console.log("putResponse:", putResponse);
 }
 
+// Run all test functions sequentially
 async function test(){
   await getComponentMethods();
   await sendGetRequest();
@@ -86,7 +96,7 @@ async function test(){
   await sendPutRequest();
 }
 
+// Execute test and log when done
 test().then(()=>{
   console.log("test done");
 })
-
